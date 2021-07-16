1d4da0fcf2f54e286c5cb972a6145e62a323bf86
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2021-07-15 21:33:17 -0300

    Desenvolvimento JavaScript e algumas atualizações nas páginas

--- javascript/script.js
+++ javascript/script.js
@@ -16,17 +16,18 @@ function enviar() { /* Função chamada ao clicar no botão enviar */
      } else if (mensagem.value.length == 0) { /* Se o campo mensagem não tiver caracteres nenhum... */
          alert('Só faltou digitar sua mensagem!')
      } else { /* Se nenhuma das opções anteriores forem verdadeiras... */
-         alert('Mensagem enviada com sucesso! Em breve retornaremos.')  
+         alert('Mensagem enviada com sucesso! Em breve retornaremos.')
+         
         } 
     }
 
-    /* ########     ↓ VERIFICADOR DE IDADE ↓     ########   */
+    /* ########     BOTÃO VERIFICADOR DE IDADE ↓     ########   */
 
-    var button = document.querySelector('button') /* Variável receber o valor do button do html*/
+    var button = document.querySelector('.button') /* Variável receber o valor do button do html*/
     var popup = document.querySelector('.popup-wrapper') /* Variável receber o valor .popup-wrapper do html*/
-    
-
-    button.addEventListener('click', () => { /* Colocar evento de click na variável button. 1º argumento 'click' e após o click o 2º argumento a função*/
+   
+    button.addEventListener('click', () => {
+     /* Colocar evento de click na variável button. 1º argumento 'click' e após o click o 2º argumento a função*/
         popup.style.display = 'block' /* Após clicar no botão aparece a janela que estava com display = none*/
     })
 
@@ -41,7 +42,7 @@ function enviar() { /* Função chamada ao clicar no botão enviar */
         }   
     })
 
-    /* ##############      ↓ ARTE CSS ↓    ############## */
+    /* ##############    BOTÃO ARTE CSS ↓    ############## */
 
     var button1 = document.querySelector('.button1') /* Variável receber o valor do button1 do html */
     var popup1 = document.querySelector('.popup1-wrapper') /* Variável receber o valor .popup1-wrapper do html */
@@ -63,3 +64,101 @@ function enviar() { /* Função chamada ao clicar no botão enviar */
         
     })
 
+/* ########### BOTÃO SITE  CONTADOR ############ */
+
+var buttonContador = document.querySelector('.buttonContador')
+var popupContador = document.querySelector('.popupContador-wrapper')
+
+buttonContador.addEventListener('click', () => {
+    popupContador.style.display = 'block'
+})
+
+popupContador.addEventListener('click', event => {
+    var classNameOfClickedElementContador = event.target.classList[0]
+    var listaCondicoesFecharContador = ['popupContador-wrapper', 'popupContadorClose', 'popupContador-link']
+    var todasCondicoesParaFecharContador = listaCondicoesFecharContador.some(listaCondicoesFecharContador => listaCondicoesFecharContador === classNameOfClickedElementContador)
+
+    if (todasCondicoesParaFecharContador) {
+        popupContador.style.display = 'none'
+    }
+    
+})
+
+/* ########### BOTÃO SITE  TABUADA ############ */
+
+var buttonTabuada = document.querySelector('#buttonTabuada')
+var popupTabuada = document.querySelector('.popupTabuada-wrapper')
+
+
+buttonTabuada.addEventListener('click', () => {
+    popupTabuada.style.display = 'block'
+})
+
+popupTabuada.addEventListener('click', event => {
+    var classNameOfClickedElementTabuada = event.target.classList[0]
+    var listaCondicoesFecharTabuada = ['popupTabuadaClose', 'popupTabuada-wrapper', 'popupTabuada-link']
+    var todasCondicoesParaFecharTabuada = listaCondicoesFecharTabuada.some(listaCondicoesFecharTabuada => listaCondicoesFecharTabuada === classNameOfClickedElementTabuada)
+
+    if (todasCondicoesParaFecharTabuada) {
+        popupTabuada.style.display = 'none'
+    }
+})
+
+/* ########### BOTÃO CÓPIA SITE IRINA BLOK ############ */
+
+var buttonIrina = document.querySelector('#buttonIrina')
+var popupIrina = document.querySelector('.popupIrina-wrapper')
+
+buttonIrina.addEventListener('click', () => {
+    popupIrina.style.display = 'block'
+})
+
+popupIrina.addEventListener('click', event => {
+    var classNameOfClickedElementIrina = event.target.classList[0]
+    var listaCondicoesFecharIrina = ['popupIrinaClose', 'popupIrina-wrapper',  'popupIrina-link']
+    var todasCondicoesParaFecharIrina = listaCondicoesFecharIrina.some(listaCondicoesFecharIrina => listaCondicoesFecharIrina === classNameOfClickedElementIrina)
+    
+    if (todasCondicoesParaFecharIrina) {
+        popupIrina.style.display = 'none'
+    } 
+})
+
+/* ########### BOTÃO SITE HORA CERTA ############ */
+
+var buttonHoraCerta = document.querySelector('#buttonHoraCerta')
+var popupHora = document.querySelector('.popupHora-wrapper')
+
+
+buttonHoraCerta.addEventListener('click', () => {
+    popupHora.style.display = 'block'
+})
+
+popupHora.addEventListener('click', event => {
+    var classNameOfClickedElementHora = event.target.classList[0]
+    listaCondicoesFecharHora = ['popupHora-wrapper', 'popupHoraClose', 'popupHora-link']
+    var todasCondicoesParaFecharHora = listaCondicoesFecharHora.some(listaCondicoesFecharHora => listaCondicoesFecharHora === classNameOfClickedElementHora)
+
+    if (classNameOfClickedElementHora) {
+        popupHora.style.display = 'none'
+    }
+})
+
+/* ########### BOTÃO SITE PESSOAL ISAIAS ############ */
+
+var buttonEsteS = document.querySelector('#buttonEsteS')
+var popupEsteS = document.querySelector('.popupEsteS-wrapper')
+
+buttonEsteS.addEventListener('click', () => {
+    popupEsteS.style.display = 'block'
+})
+
+popupEsteS.addEventListener('click', event => {
+    var classNameOfClickedElementEsteS = event.target.classList[0]
+    var listaCondicoesFecharEsteS = ['popupEsteSClose', 'popupEsteS-wrapper', 'popupEsteS-link']
+    var todasCondicoesParaFecharEsteS = listaCondicoesFecharEsteS.some(listaCondicoesFecharEsteS => listaCondicoesFecharEsteS === classNameOfClickedElementEsteS)
+
+    if (classNameOfClickedElementEsteS) {
+       popupEsteS.style.display = 'none'
+    }
+    
+})
